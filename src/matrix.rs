cc78b902c205d991d2271255a80fa6838404a90a
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-04-04 11:34:58 -0700

    Implement pack_into

--- src/matrix.rs
+++ src/matrix.rs
@@ -61,6 +61,24 @@ impl Matrix {
     pub(crate) fn insert(&mut self, i: usize, j: usize, val: Value) {
         self.inner[j * self.row + i] = val;
     }
+
+    /// Returns a copied matrix must be smaller than the original
+    /// The range ends must be exclusive
+    pub(crate) fn pack_into(
+        &self,
+        i_from: usize,
+        i_to: usize,
+        j_from: usize,
+        j_to: usize,
+    ) -> Matrix {
+        let mut copy = Matrix::new(i_to - i_from, j_to - j_from);
+        for i in i_from..i_to {
+            for j in j_from..j_to {
+                copy.insert(i, j, self.get(i, j));
+            }
+        }
+        copy
+    }
 }
 
 impl Display for Matrix {

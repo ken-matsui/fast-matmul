68f6f343a8162ddbd75c4f8068bb8fd0cd5288bf
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-08 21:33:42 -0700

    Move initializations out of the benchmarks

--- benches/naive-vs-fast.rs
+++ benches/naive-vs-fast.rs
@@ -4,32 +4,24 @@ use fast_matmul::{fast::Param, *};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
-fn naive_matmul(size: usize) {
-    let A = Matrix::random_new(size, size);
-    let B = Matrix::random_new(size, size);
-    let mut C = Matrix::new(size, size);
-    naive::matmul(size, size, size, &A, &B, &mut C);
-}
-fn fast_matmul(size: usize, param: Param) {
-    let A = Matrix::random_new(size, size);
-    let B = Matrix::random_new(size, size);
-    let mut C = Matrix::new(size, size);
-    fast::matmul(size, size, size, &A, &B, &mut C, param);
-}
-
 fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("Matmul");
     group.sample_size(10); // 10 is minimum required; default is 100
 
-    for parameter in [2048].iter() {
-        group.throughput(Throughput::Elements(*parameter as u64));
+    for size in [2048].iter() {
+        group.throughput(Throughput::Elements(*size as u64));
 
         let param = Param::new(128, 1024, 1);
-        group.bench_with_input(BenchmarkId::new("fast", parameter), parameter, |b, par| {
-            b.iter(|| fast_matmul(*par, param))
+        let A = Matrix::random_new(*size, *size);
+        let B = Matrix::random_new(*size, *size);
+
+        group.bench_with_input(BenchmarkId::new("fast", size), size, |b, size| {
+            let mut C = Matrix::new(*size, *size);
+            b.iter(|| fast::matmul(*size, *size, *size, &A, &B, &mut C, param))
         });
-        group.bench_with_input(BenchmarkId::new("naive", parameter), parameter, |b, par| {
-            b.iter(|| naive_matmul(*par))
+        group.bench_with_input(BenchmarkId::new("naive", size), size, |b, size| {
+            let mut C = Matrix::new(*size, *size);
+            b.iter(|| naive::matmul(*size, *size, *size, &A, &B, &mut C))
         });
     }
     group.finish();

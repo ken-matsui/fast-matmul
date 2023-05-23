1b01e8935e20d32e93b3ec64a42e467fac10b30e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-23 13:04:22 -0700

    create src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![allow(non_snake_case)]
+
+use fast_matmul::*;
+
+const SIZE: usize = 2048;
+
+fn main() {
+    let A = Matrix::rand_new(SIZE, SIZE);
+    let B = Matrix::rand_new(SIZE, SIZE);
+    let mut C = Matrix::zero_new(SIZE, SIZE);
+
+    fast::best_matmul(SIZE, SIZE, SIZE, &A, &B, &mut C);
+}

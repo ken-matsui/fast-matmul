548ff3bd09c0500af89bfe93b837e622c843dc1d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-26 13:24:40 -0700

    add test for trans

--- src/trans.rs
+++ src/trans.rs
@@ -36,7 +36,27 @@ mod tests {
     }
 
     #[test]
-    fn test_trans_impl() {
+    fn test_trans_impl_1() {
+        let mut size: usize = 2;
+
+        let mut A = Matrix::zero_new(size, size);
+        A.insert(0, 0, 21);
+        A.insert(1, 0, 53);
+        A.insert(0, 1, 7);
+        A.insert(1, 1, 3);
+
+        let mut expected = Matrix::zero_new(size, size);
+        expected.insert(0, 0, 21);
+        expected.insert(1, 0, 7);
+        expected.insert(0, 1, 53);
+        expected.insert(1, 1, 3);
+
+        let At = A.transpose();
+        assert_eq!(At, expected);
+    }
+
+    #[test]
+    fn test_trans_impl_2() {
         let mut size: usize = 2;
         loop {
             let A = Matrix::rand_new(size, size);

80b6dfe16b1b423c3face20125e15e845539f9a1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-26 01:33:34 -0700

    additional pack methods

--- src/matrix.rs
+++ src/matrix.rs
@@ -6,7 +6,7 @@ use mimalloc::MiMalloc;
 static GLOBAL: MiMalloc = MiMalloc;
 // `Vec` will allocate memory through `GLOBAL` above
 
-/// Value type for matrix
+/// Value type for Matrix
 type Value = u32;
 
 #[derive(Debug, PartialEq)]
@@ -19,6 +19,7 @@ pub struct Matrix {
 
 impl Matrix {
     /// Initialize with 0
+    #[inline(always)]
     pub fn zero_new(row: usize, col: usize) -> Self {
         Matrix {
             row,
@@ -28,6 +29,7 @@ impl Matrix {
         }
     }
     /// Initialize with 1
+    #[inline(always)]
     pub fn one_new(row: usize, col: usize) -> Self {
         Matrix {
             row,
@@ -50,6 +52,7 @@ impl Matrix {
         matrix
     }
     /// Initialize with random numbers
+    #[inline(always)]
     pub fn rand_new(row: usize, col: usize) -> Self {
         Matrix {
             row,
@@ -95,10 +98,23 @@ impl Matrix {
     ///
     /// For better performance, this method does not check if arguments are out of bounds.
     pub fn pack_into(&self, i_from: usize, i_to: usize, j_from: usize, j_to: usize) -> Matrix {
+        self.pack_row_mjr(i_from, i_to, j_from, j_to)
+    }
+
+    pub fn pack_row_mjr(&self, i_from: usize, i_to: usize, j_from: usize, j_to: usize) -> Matrix {
         let mut copy = Matrix::zero_new(i_to - i_from, j_to - j_from);
         for j in j_from..j_to {
             for i in i_from..i_to {
                 copy.insert(i - i_from, j - j_from, *self.get(i, j));
+            }
+        }
+        copy
+    }
+    pub fn pack_col_mjr(&self, i_from: usize, i_to: usize, j_from: usize, j_to: usize) -> Matrix {
+        let mut copy = Matrix::zero_new(i_to - i_from, j_to - j_from);
+        for j in j_from..j_to {
+            for i in i_from..i_to {
+                copy.insert(i - i_from, j - j_from, *self.get(j, i));
             }
         }
         copy
@@ -114,5 +130,56 @@ impl Display for Matrix {
             writeln!(f)?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pack_row_mjr_just_copy() {
+        let matrix = Matrix::seq_new(4, 4);
+        let copy = matrix.pack_row_mjr(0, 4, 0, 4);
+        assert_eq!(copy, matrix);
+    }
+
+    #[test]
+    fn test_pack_row_mjr_empty() {
+        let matrix = Matrix::seq_new(4, 4);
+        let copy = matrix.pack_row_mjr(0, 0, 0, 0);
+        assert_eq!(copy, Matrix::zero_new(0, 0));
+    }
+
+    #[test]
+    fn test_pack_row_mjr_1() {
+        let matrix = Matrix::seq_new(4, 4);
+        let copy = matrix.pack_row_mjr(0, 4, 0, 1);
+        assert_eq!(copy.inner, Matrix::seq_new(2, 2).inner);
+    }
+    #[test]
+    fn test_pack_row_mjr_2() {
+        let matrix = Matrix::seq_new(4, 4);
+        let copy = matrix.pack_row_mjr(0, 4, 0, 2);
+        assert_eq!(copy.inner, Matrix::seq_new(4, 2).inner);
+    }
+    #[test]
+    fn test_pack_row_mjr_3() {
+        let matrix = Matrix::seq_new(4, 4);
+        let copy = matrix.pack_row_mjr(0, 4, 2, 4);
+
+        let start = 2 * 4 + 0; // j * row + i
+        for i in 0..4 {
+            for j in 0..2 {
+                assert_eq!(*copy.get(i, j), (start + i + j * 4) as u32);
+            }
+        }
+    }
+
+    #[test]
+    fn test_pack_col_mjr_just_copy() {
+        let matrix = Matrix::seq_new(4, 4);
+        let copy = matrix.pack_col_mjr(0, 4, 0, 4);
+        assert_eq!(copy, matrix.trans(), "copy should be transposed");
     }
 }

f4604673bee0463009a079ac572e6e2784409670
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-28 06:07:04 -0700

    matmul_with_trans

--- src/fast.rs
+++ src/fast.rs
@@ -37,6 +37,41 @@ pub fn matmul(A: &Matrix, B: &Matrix, C: &mut Matrix, param: Param) {
     }
 }
 
+pub fn matmul_with_trans(A: &Matrix, B: &Matrix, C: &mut Matrix, param: Param) {
+    let m = A.height /* = C.height */;
+    let k = B.height /* = A.width */;
+    let n = C.width /* = B.width */;
+
+    for jc in (0..n).step_by(param.nc) {
+        for pc in (0..k).step_by(param.kc) {
+            let ik = min(pc + param.kc, k);
+            // FIXME: assuming B is square
+            let Bc = B.pack_into_col_major(pc, ik, jc, min(jc + param.nc, n));
+
+            for ic in (0..m).step_by(param.mc) {
+                let Ac = A.pack_into(ic, min(ic + param.mc, m), pc, ik);
+                //
+                // Macrokernel
+                //
+                for ir in (0..param.mc).step_by(Ac.height /* mr */) {
+                    for jr in (0..param.nc).step_by(Bc.height /* nr */) {
+                        //
+                        // Microkernel
+                        //
+                        for pr in 0..min(param.kc, Ac.width /* = Bc.width */) {
+                            for i in ir..Ac.height {
+                                for j in jr..Bc.height {
+                                    *C.get_mut(i + ic, j + jc) += Ac.get(i, pr) * Bc.get(j, pr);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 // NOTE: optimized for BEST_PARAM
 pub fn simd_matmul(A: &Matrix, B: &Matrix, C: &mut Matrix, param: Param) {
     let m = A.height /* = C.height */;
@@ -92,7 +127,8 @@ pub fn simd_matmul(A: &Matrix, B: &Matrix, C: &mut Matrix, param: Param) {
                                 }
                                 #[cfg(target_arch = "aarch64")]
                                 {
-                                    *C.get_mut(i + ic, j + jc) += Ac.get(i, 0) * Bc.get(0, j);
+                                    let pr = 0;
+                                    *C.get_mut(i + ic, j + jc) += Ac.get(i, pr) * Bc.get(j, pr);
                                 }
                             }
                         }
@@ -115,20 +151,6 @@ mod tests {
     use crate::*;
     use itertools::iproduct;
 
-    #[test]
-    fn test_matmul_1() {
-        let m: usize = 8;
-        let k: usize = 8;
-        let n: usize = 8;
-
-        let A = Matrix::seq_new(m, k);
-        let B = Matrix::seq_new(k, n);
-        let mut C = Matrix::zero_new(m, n);
-
-        fast::matmul(&A, &B, &mut C, Param::new(1, 1, 1));
-        assert_eq!(C, expected_8x8());
-    }
-
     fn matmul_helper(size: usize, param: Param) {
         let m: usize = size;
         let k: usize = size;
@@ -147,16 +169,41 @@ mod tests {
     }
 
     #[test]
-    fn test_simple_matmul() {
+    fn test_matmul_1() {
+        let m: usize = 8;
+        let k: usize = 8;
+        let n: usize = 8;
+
+        let A = Matrix::seq_new(m, k);
+        let B = Matrix::seq_new(k, n);
+        let mut C = Matrix::zero_new(m, n);
+
+        fast::matmul(&A, &B, &mut C, Param::new(1, 1, 1));
+        assert_eq!(C, expected_8x8());
+    }
+    #[test]
+    fn test_matmul_2() {
+        let mut i: usize = 2;
+        loop {
+            matmul_helper(i, Param::default());
+            i = i.pow(2);
+
+            if i > 128 {
+                break;
+            }
+        }
+    }
+
+    #[test]
+    fn test_matmul_no_blocking_1() {
         // simple cases: until the smallest step
         // no step_by occurs, meaning this is the same with iterating by 1
         for size in 1..Param::default().kc {
             matmul_helper(size, Param::default());
         }
     }
-
     #[test]
-    fn test_naive_matmul_1() {
+    fn test_matmul_no_blocking_2() {
         let size = 4;
 
         let A = Matrix::rand_new(size, size);
@@ -167,27 +214,13 @@ mod tests {
         let mut C2 = Matrix::zero_new(size, size);
         fast::matmul(&A, &B, &mut C2, Param::new(1, 1, 1));
     }
-
     #[test]
-    fn test_naive_matmul_2() {
+    fn test_matmul_no_blocking_3() {
         let naive_param = Param::new(1, 1, 1);
 
         let mut i: usize = 2;
         loop {
             matmul_helper(i, naive_param);
-            i = i.pow(2);
-
-            if i > 128 {
-                break;
-            }
-        }
-    }
-
-    #[test]
-    fn test_matmul_2() {
-        let mut i: usize = 2;
-        loop {
-            matmul_helper(i, Param::default());
             i = i.pow(2);
 
             if i > 128 {
@@ -208,7 +241,6 @@ mod tests {
             matmul_helper(256, param);
         }
     }
-
     #[test]
     fn test_large_matmul() {
         matmul_helper(2048, BEST_PARAM);
@@ -250,6 +282,101 @@ mod tests {
         let mut C2 = Matrix::zero_new(size, size);
         naive::matmul(&A, &B, &mut C2);
         assert_eq!(C, C2);
+    }
+
+    fn matmul_with_trans_helper(size: usize, param: Param) {
+        let m: usize = size;
+        let k: usize = size;
+        let n: usize = size;
+
+        let A = Matrix::rand_new(m, k);
+        let B = Matrix::rand_new(k, n);
+
+        let mut C = Matrix::zero_new(m, n);
+        fast::matmul_with_trans(&A, &B, &mut C, param);
+
+        let mut C2 = Matrix::zero_new(m, n);
+        naive::matmul(&A, &B, &mut C2);
+
+        assert_eq!(C, C2);
+    }
+
+    #[test]
+    fn test_matmul_with_trans_1() {
+        let m: usize = 8;
+        let k: usize = 8;
+        let n: usize = 8;
+
+        let A = Matrix::seq_new(m, k);
+        let B = Matrix::seq_new(k, n);
+        let mut C = Matrix::zero_new(m, n);
+
+        fast::matmul_with_trans(&A, &B, &mut C, Param::new(1, 1, 1));
+        assert_eq!(C, expected_8x8());
+    }
+    #[test]
+    fn test_matmul_with_trans_2() {
+        let mut i: usize = 2;
+        loop {
+            matmul_with_trans_helper(i, Param::default());
+            i = i.pow(2);
+
+            if i > 128 {
+                break;
+            }
+        }
+    }
+
+    #[test]
+    fn test_matmul_with_trans_no_blocking_1() {
+        // simple cases: until the smallest step
+        // no step_by occurs, meaning this is the same with iterating by 1
+        for size in 1..Param::default().kc {
+            matmul_with_trans_helper(size, Param::default());
+        }
+    }
+    #[test]
+    fn test_matmul_with_trans_no_blocking_2() {
+        let size = 4;
+
+        let A = Matrix::rand_new(size, size);
+        let B = Matrix::rand_new(size, size);
+        let mut C = Matrix::zero_new(size, size);
+        naive::matmul_with_trans(&A, &B, &mut C);
+
+        let mut C2 = Matrix::zero_new(size, size);
+        fast::matmul(&A, &B, &mut C2, Param::new(1, 1, 1));
+    }
+    #[test]
+    fn test_matmul_with_trans_no_blocking_3() {
+        let naive_param = Param::new(1, 1, 1);
+
+        let mut i: usize = 2;
+        loop {
+            matmul_with_trans_helper(i, naive_param);
+            i = i.pow(2);
+
+            if i > 128 {
+                break;
+            }
+        }
+    }
+
+    #[test]
+    fn test_small_pack_matmul_with_trans() {
+        let range = 0..=8; // 2^(0..=8) = 1..=256
+        for (nc_exp, mc_exp, kc_exp) in iproduct!(range.clone(), range.clone(), range) {
+            let param = Param::new(
+                2_usize.pow(nc_exp),
+                2_usize.pow(mc_exp),
+                2_usize.pow(kc_exp),
+            );
+            matmul_with_trans_helper(256, param);
+        }
+    }
+    #[test]
+    fn test_large_matmul_with_trans() {
+        matmul_with_trans_helper(2048, BEST_PARAM);
     }
 
     // #[test]
